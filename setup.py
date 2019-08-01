d3e0b165ef613cbe907dc9c80cc0178ddd6091e5
RedBanies3ofThem <jonathan.m.sadighian@gmail.com>
2019-08-01 23:38:41 +0200

    Initial commit Part-3: 1) Completed data download and import utility classes 2) Added test cases for utility classes 3) Updated `.gitignore`

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from setuptools import setup
 import os
 
 
-cwd = os.path.dirname(os.path.realpath(__file__))
-with open('{}/requirements.txt'.format(cwd)) as f:
+cwd = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requirements.txt')
+with open(cwd) as f:
     dependencies = list(map(lambda x: x.replace("\n", ""), f.readlines()))
 
 
 setup(
-    name='reco-gym',
-    version='1.0',
+    name='recommendation-gym',
+    version='1.1',
     url='https://github.com/RedBanies3ofThem/recommendation-gym',
     license='Apache 2.0',
     author='Jonathan Sadighian',
     author_email='jonathan.m.sadighian@gmail.com',
-    description='POMDP recommendation system framework for MovieLens dataset',
+    description='POMDP recommendation system framework for MovieLens data set',
     install_requires=dependencies,
     packages=['gym_recommendation'],
 )

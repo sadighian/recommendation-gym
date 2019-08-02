94faada7f33762ccf2657e91b5198385ddf7d22f
RedBanies3ofThem <jonathan.m.sadighian@gmail.com>
2019-08-02 19:27:55 +0200

    Initial commit Part-5: 1) Added `experimeny.py` to launch algorithm 2) Updated `README.md`

--- README.md
+++ README.md
@@ -1,4 +1,46 @@
-# recommendation-gym
-MovieLens recommendation system using OpenAI's GYM framework
-
+# Recommendation Gym for MovieLens
 **Under construction as of 1-Aug-2019**
+## 1. Overview
+
+### 1.1 Summary
+The purpose of this project was to experiment with the application
+of deep reinforcement learning to recommendation systems.
+More specifically, this project applies `Stable-Baselines`
+algorithms to the `MovieLens 100k` data set.
+
+To that end, the goal of the agent is to predict what rating 
+a `user` will give to a given `movie`. 
+
+### 1.2 Implementation
+
+#### State
+The observation space is comprised of data from the `MovieLens` data set:
+- **user_mean:** Average rating given by a specific user_id
+- **movie_mean:** Average rating for a specific movie_id
+- **movie_genre_bucket:** One-hot of the movie type
+- **age_bucket:** One-hot of user's age range
+- **occupation_bucket:** One-hot of the user's job
+- **gender_bucket:** One-hot of the user's gender (only M or F)
+
+#### Reward
+The reward structure is derived from the distance between the 
+Agent's predicted rating vs. `user`'s actual rating.
+
+
+## 2. Project Structure
+```
+gym_recommendation/
+    data/           ...MovieLens 100k data set
+    envs/
+    tests/
+    utils.py
+experimeny.py
+requirements.txt
+setup.py
+```
+
+## 3. Getting Started
+To be added
+
+## 4. Appendix
+To be added
---
+++ experiment.py
@@ -0,0 +1,125 @@
+import gym
+import gym_recommendation
+from datetime import datetime as dt
+from stable_baselines.common.policies import *
+from stable_baselines.common.vec_env import SubprocVecEnv
+from stable_baselines import *
+import os
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--learning_rate',
+                    default=float(3e-4),
+                    help="Learning rate for optimizer",
+                    type=float)
+parser.add_argument('--n_steps',
+                    default=64,
+                    help="Number of steps for PPO to roll out",
+                    type=int)
+parser.add_argument('--tensorboard_log',
+                    default="./ppo2_recommendations",
+                    help="Logging directory for TensorBoard log",
+                    type=str)
+parser.add_argument('--save_model',
+                    default=True,
+                    help="Save model after training (True = yes | False = no)",
+                    type=bool)
+parser.add_argument('--training_steps',
+                    default=int(1e6),
+                    help="Number of steps to performing training",
+                    type=int)
+parser.add_argument('--evaluation_steps',
+                    default=int(1e5),
+                    help="Number of steps to performing on evaluation",
+                    type=int)
+parser.add_argument('--seed',
+                    default=1,
+                    help="Random number seed for evaluation",
+                    type=int)
+parser.add_argument('--num_of_layers',
+                    default=2,
+                    help="Number of hidden layers in the MLP",
+                    type=int)
+parser.add_argument('--num_of_neurons',
+                    default=64,
+                    help="Number of neurons in each hidden layer",
+                    type=int)
+parser.add_argument('--nminibatches',
+                    default=4,
+                    help="Number of mini-batches in PPO",
+                    type=int)
+kwargs = vars(parser.parse_args())
+
+
+def evaluate(model, env, num_steps=1000):
+    """
+    Evaluate a RL agent
+    """
+    start_time = dt.now()
+    obs = env.reset()
+    step_count = 0
+    episode_number = 1
+    for i in range(num_steps):
+        step_count += 1
+        action, _states = model.predict(obs)
+        obs, reward, done, info = env.step(action)
+        if done:
+            elapsed = (dt.now() - start_time).seconds
+            print("**************EPISODE #{}****************".format(episode_number))
+            print("Total steps=", step_count, "steps/second=", step_count / elapsed)
+            print("Total correct predictions=", env.total_correct_predictions)
+            print("Prediction accuracy=", env.total_correct_predictions / step_count)
+            obs = env.reset()
+            step_count = 0
+            episode_number += 1
+            start_time = dt.now()
+
+
+def main(kwargs):
+    start_time = dt.now()
+    number_of_cpu = os.cpu_count()
+    envs = SubprocVecEnv([lambda: gym.make(gym_recommendation.RecoEnv.id,
+                                           **gym_recommendation.import_data_for_env())
+                          for _ in range(number_of_cpu)])
+    ppo_configs = {
+        'learning_rate': kwargs['learning_rate'],  # lambda f: f * float(3e-4),
+        'n_steps': kwargs['n_steps'],
+        'tensorboard_log': kwargs['tensorboard_log'],
+        'nminibatches': min(number_of_cpu, kwargs['nminibatches']),
+        'noptepochs': number_of_cpu,
+        'policy_kwargs': dict(net_arch=[kwargs['num_of_neurons']]*kwargs['num_of_layers']),
+        'gamma': 0.0,
+        'ent_coef': 0.01,
+        'vf_coef': 0.5,
+        'max_grad_norm': 0.5,
+        'lam': 0.95,
+        'cliprange': 0.2,
+        'verbose': 0,
+        '_init_setup_model': True,
+    }
+    print('*********************************')
+    print("ppo_configs:", ppo_configs)
+    print('*********************************')
+
+    model = PPO2(MlpPolicy, envs, **ppo_configs)
+    model.learn(total_timesteps=kwargs['training_steps'], seed=kwargs['seed'])
+    elapsed = (dt.now() - start_time).seconds
+    print("Finished training in {} seconds".format(elapsed))
+
+    if kwargs['save_model']:
+        save_name = 'PPO2_Recommendations'
+        print('Saving PPO model as {}'.format(save_name))
+        model.save(save_name)
+
+    evaluate(model=model,
+             env=gym.make(gym_recommendation.RecoEnv.id,
+                          **gym_recommendation.import_data_for_env()),
+             num_steps=kwargs['evaluation_steps'])
+    elapsed = (dt.now() - start_time).seconds
+    print("Finished training AND evaluation in {} seconds".format(elapsed))
+
+
+if __name__ == "__main__":
+    print("Starting experiment with PPO2 at {}".format(dt.now()))
+    main(kwargs=kwargs)
